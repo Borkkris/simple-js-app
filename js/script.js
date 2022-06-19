1b3d72827cfdf471f1d2bdfc04e9c1fbbba478ab
Borkkris <Borkkris@users.noreply.github.com>
2022-06-19 21:39:12 +0200

    Update script.js
    added a new pokemon (Onix)
    added a for loop
    
    added a condition to show which one is a big, small and normal sized Pokemon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 let pokemonList=[
-    {name:'Bulbasaur', height:7, type:['grass', 'poison']},
-    {name:'Charmander', height:6, type:'fire'},
-    {name:'Squirtle', height:5, type:'water'}
+    {name:'Bulbasaur', height:2.04, type:['grass', 'poison']},
+    {name:'Charmander', height:2, type:'fire'},
+    {name:'Squirtle', height:1.08, type:'water'},
+    {name:'Onix', height: 28.10, type:['rock','ground']}
 ]
+let i = 0;
+for (let i=0 ; i < pokemonList.length; i++);
+document.write(pokemonList[i].name)
+
+
+for (let i=0; i<pokemonList.length; i++){
+    if(pokemonList[i].height >20){
+    console.log(pokemonList[i].height + "Wow thats a big Pokemon");
+}   else if(pokemonList[i].height <6){
+    console.log(pokemonList[i].height + "small Pokemon");
+}   else {
+    console.log(pokemonList[i].height + "normal sized Pokemon");
+}
+}
