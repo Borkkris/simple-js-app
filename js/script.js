de53a95b9cc9a8bdad44a29eb995c0edc834023c
Borkkris <Borkkris@users.noreply.github.com>
2022-06-19 22:10:08 +0200

    Update script.js
    added brackets
    its now shown in index.html

--- js/script.js
+++ js/script.js
@@ -5,16 +5,17 @@ let pokemonList=[
     {name:'Onix', height: 28.10, type:['rock','ground']}
 ]
 
-for (let i = 0 ; i < pokemonList.length; i++);
-document.write(pokemonList[i].name)
+for (let i = 0 ; i < pokemonList.length; i++){
+document.write(pokemonList[i].name + " ");
+}
 
 
-for (let a=0; a<pokemonList.length; a++){
-    if(pokemonList[a].height >20){
-    console.log(pokemonList[a].height + "Wow thats a big Pokemon");
-}   else if(pokemonList[i].height <6){
-    console.log(pokemonList[a].height + "small Pokemon");
+for (let i=0; i < pokemonList.length; i++){
+    if(pokemonList[i].height >20){
+    document.write(pokemonList[i].height + "Wow, thats a big Pokemon!");
+}   else if(pokemonList[i].height <2){
+    document.write(pokemonList[i].height + "small Pokemon");
 }   else {
-    console.log(pokemonList[a].height + "normal sized Pokemon");
+    document.write(pokemonList[i].height + "normal sized Pokemon");
 }
 }
