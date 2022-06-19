b1fa6991d738e226bdb9cc0e3b616a2b79b49004
Borkkris <Borkkris@users.noreply.github.com>
2022-06-19 21:44:13 +0200

    Update script.js
    some minor changes

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ let pokemonList=[
     {name:'Squirtle', height:1.08, type:'water'},
     {name:'Onix', height: 28.10, type:['rock','ground']}
 ]
-let i = 0;
-for (let i=0 ; i < pokemonList.length; i++);
+
+for (let i = 0 ; i < pokemonList.length; i++);
 document.write(pokemonList[i].name)
 
 
-for (let i=0; i<pokemonList.length; i++){
-    if(pokemonList[i].height >20){
-    console.log(pokemonList[i].height + "Wow thats a big Pokemon");
+for (let a=0; a<pokemonList.length; a++){
+    if(pokemonList[a].height >20){
+    console.log(pokemonList[a].height + "Wow thats a big Pokemon");
 }   else if(pokemonList[i].height <6){
-    console.log(pokemonList[i].height + "small Pokemon");
+    console.log(pokemonList[a].height + "small Pokemon");
 }   else {
-    console.log(pokemonList[i].height + "normal sized Pokemon");
+    console.log(pokemonList[a].height + "normal sized Pokemon");
 }
 }
